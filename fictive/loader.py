bd085a270b7436e298e87a35d63cf2751c32ac56
Remy <t3knomanser@gmail.com>
2025-09-09 10:51:38 -0400

    Removed PyYAML, Switched to Ruamel.Yaml
    I've gone back and forth on this a few times, but PyYAML doesn't seem
    to have YAML1.2 support, and the result is that "on" and "off" behave
    in the stupid YAML fashion. Ruamel is YAML1.2, and this fixes the issues
    I was having with the example game with only minor changes to my code,
    instead of trying to rewrite the entire goddamn type coercion and likely
    running into some sort of mistake there, because I hate type coercion.
    
    I do suspect this will still bite me at some point with boolean values, but we'll see.

--- fictive/loader.py
+++ fictive/loader.py
@@ -1,7 +1,9 @@
-import yaml
+from ruamel.yaml import YAML
 from pathlib import Path
 from contextlib import ExitStack
 from typing import List, Iterator, Tuple
+
+yaml=YAML(typ='safe')   # default,
 
 def merge_game_yaml(root: Path, manifest: Path) -> Tuple[dict, str]:
     """
@@ -10,7 +12,7 @@ def merge_game_yaml(root: Path, manifest: Path) -> Tuple[dict, str]:
     """
     if manifest.exists():
         with manifest.open() as f:
-            mfest = yaml.load(f, yaml.SafeLoader)
+            mfest = yaml.load(f)
             files = [root / entry for entry in mfest["files"]]
         with ExitStack() as stack:
             read_from = [stack.enter_context(
@@ -27,12 +29,12 @@ def load_game_yaml(gameInstance: Path | str):
     root = Path(gameInstance).resolve()
     if not root.is_dir():  # the game is in a single file
         with root.open() as f:
-            return yaml.load(f, yaml.SafeLoader)
+            return yaml.load(f)
     # otherwise, the game is in a directory, look for a manifest
     manifest = root / "manifest.yaml"
     mfest, merged = merge_game_yaml(root, manifest)
     if merged:
-        loaded = yaml.load(merged, yaml.SafeLoader)
+        loaded = yaml.load(merged)
         return loaded + [{key: value} for key, value in mfest.items()]
     return None
 
@@ -46,6 +48,6 @@ def load_manifest_yaml(gameInstance: Path | str):
     manifest = root / "manifest.yaml"
     if manifest.exists():
         with manifest.open() as f:
-            loaded = yaml.load(f, yaml.SafeLoader)
+            loaded = yaml.load(f)
         return loaded.get("title", "A Game"), loaded.get("slug", "Slug for a game"), loaded.get("author", "Anonymous")
     return None
