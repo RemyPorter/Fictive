1e55a33c026dcf6f456f0a3cb4f8a61839741219
Remy <t3knomanser@gmail.com>
2025-09-15 19:54:31 -0400

    BIG Refactor Moment: Ditching YAML Lists
    In retrospect, it was extremely dumb to organize our YAML as a list, instead of a dict.
    
    Since the syntax is now more set, there's now a dev guide document, which likely needs
    some TLC in the future, but gives developers a picture of how to write Fictive games.

--- fictive/test_runner.py
+++ fictive/test_runner.py
@@ -30,23 +30,20 @@ def run_tests(test:dict, machine:Machine, statebag:Statebag)->Dict[str, List[boo
     """Run a series of tests against a state machine"""
     results:Dict[str, List[bool]] = {}
     for name in test.keys():
-        if name == "failed_test":
-            breakpoint()
         parsed = parse_test(name, test[name])
         results[name] = parsed.run(machine, statebag)
     return results
 
-def test_main(loaded:list, root: Path):
+def test_main(loaded:dict, root: Path):
     """
     Handle the loading and executions of our test scripts, print
     the results.
     """
     machine,statebag,title = parse(loaded)
-    for l in loaded[::-1]: # search for a test entry, backwards because we favor putting it an the end in parsing
-        if "tests" in l:
-            for t in l["tests"]:
-                loaded_test = load_test(root / Path(t))
-                res = run_tests(loaded_test,machine,statebag)
-                print_test_results(loaded_test, res)
+    if "tests" in loaded:
+        for t in loaded["tests"]:
+            loaded_test = load_test(root / Path(t))
+            res = run_tests(loaded_test,machine,statebag)
+            print_test_results(loaded_test, res)
 
 
